85c8cdfeb76f3190fd2f92d799035ca568ff83a7
Aneta Di Scala <38839156+netka99@users.noreply.github.com>
2020-12-20 22:44:09 +0100

    first upload

---
+++ designs.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", () => {
+
+    function navSlide() {
+        const burger = document.querySelector(".burger");
+        const nav = document.querySelector(".nav-links");
+        const navLinks = document.querySelectorAll(".nav-links li");
+
+        burger.addEventListener("click", () => {
+            nav.classList.toggle("nav-active");
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.8}s`
+            }
+        });
+
+        //Burger animation
+        burger.classList.toggle("burgerLines");
+
+        });
+
+
+    }
+
+    navSlide();
+});
